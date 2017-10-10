72935fcb50f80c66c0f1ec149428ee6dade7f6e2
Nauman Ahmad Bajwa <drnauman2016@gmail.com>
2017-10-10 00:15:52 -0400

    Add files via upload

---
+++ Exclusive/commercial-ii.php
@@ -0,0 +1,128 @@
+<?php 
+	
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+		
+		<script type="text/javascript" src="includes/jquery-3.2.1.js"></script>
+		<link rel="stylesheet" type="text/css" href="style.css" media="all" />
+	</head>
+	<body>
+	<?php include ("includes/menu.html");?>
+	
+		<div class="container"> 
+			<div class="row"> 
+				<div class="col-sm-3"> 
+					<img src="img/45.jpg" class="img-fluid" alt="Responsive image">
+					
+					</div>
+				<div class="col-sm-3"> 
+					<img src="img/46.jpg" class="img-fluid" alt="Responsive image">
+					</div>
+				<div class="col-sm-3"> 
+					<img src="img/47.jpg" class="img-fluid" alt="Responsive image">
+					</div>
+				<div class="col-sm-3"> 
+					<img src="img/48.jpg" class="img-fluid" alt="Responsive image">
+					</div>
+				
+				</div>
+			<div class="row"> 
+				<div class="col-sm-3"> 
+					<img src="img/49.jpg" class="img-fluid" alt="Responsive image">
+					
+					</div>
+				<div class="col-sm-3"> 
+					<img src="img/50.jpg" class="img-fluid" alt="Responsive image">
+					</div>
+				<div class="col-sm-3"> 
+					<img src="img/51.jpg" class="img-fluid" alt="Responsive image">
+					</div>
+				<div class="col-sm-3"> 
+					<img src="img/52.jpg" class="img-fluid" alt="Responsive image">
+					</div>
+				
+				</div>
+				<div class="row"> 
+				<div class="col-sm-3"> 
+					<img src="img/53.jpg" class="img-fluid" alt="Responsive image">
+					
+					</div>
+				<div class="col-sm-3"> 
+					<img src="img/54.jpg" class="img-fluid" alt="Responsive image">
+					</div>
+				<div class="col-sm-3"> 
+					<img src="img/55.jpg" class="img-fluid" alt="Responsive image">
+					</div>
+				<div class="col-sm-3"> 
+					<img src="img/56.jpg" class="img-fluid" alt="Responsive image">
+					</div>
+				
+				</div>
+				<div class="row"> 
+				<div class="col-sm-3"> 
+					<img src="img/57.jpg" class="img-fluid" alt="Responsive image">
+					
+					</div>
+				<div class="col-sm-3"> 
+					<img src="img/58.jpg" class="img-fluid" alt="Responsive image">
+					</div>
+				<div class="col-sm-3"> 
+					<img src="img/59.jpg" class="img-fluid" alt="Responsive image">
+					</div>
+				<div class="col-sm-3"> 
+					<img src="img/60.jpg" class="img-fluid" alt="Responsive image">
+					</div>
+				
+				</div>
+				<div class="row"> 
+				<div class="col-sm-3"> 
+					<img src="img/61.jpg" class="img-fluid" alt="Responsive image">
+					
+					</div>
+				<div class="col-sm-3"> 
+					<img src="img/62.jpg" class="img-fluid" alt="Responsive image">
+					</div>
+				<div class="col-sm-3"> 
+					<img src="img/63.jpg" class="img-fluid" alt="Responsive image">
+					</div>
+				<div class="col-sm-3"> 
+					<img src="img/64.jpg" class="img-fluid" alt="Responsive image">
+					</div>
+				
+				</div>
+				<div class="row"> 
+				<div class="col-sm-3"> 
+					<img src="img/65.jpg" class="img-fluid" alt="Responsive image">
+					
+					</div>
+				<div class="col-sm-3"> 
+					<img src="img/66.jpg" class="img-fluid" alt="Responsive image">
+					</div>
+				<div class="col-sm-3"> 
+					<img src="img/67.jpg" class="img-fluid" alt="Responsive image">
+					</div>
+				
+				
+				</div>
+				
+			</div>
+		
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+		<script src="https://use.fontawesome.com/97a933fe64.js"></script>
+	</body>
+</html>
---
+++ Exclusive/latest-news.php
@@ -0,0 +1,82 @@
+<?php 
+	
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+		
+		<script type="text/javascript" src="includes/jquery-3.2.1.js"></script>
+		<link rel="stylesheet" type="text/css" href="style.css" media="all" />
+	</head>
+	<body>
+	<?php include ("includes/menu.html");?>
+		
+		<div class="container"> 
+			<div class="row"> 
+				<div class="col-sm-12"> 
+					 <h1 class="title">Latest News </h1>
+					</div>
+				</div>
+			</div>
+			<div class="container"> 
+				<div class="row"> 
+					<div class="col-sm-3"> 
+						<img src="img/91.jpg" class="img-fluid" alt="Responsive image">
+					</div>
+					<div class="col-sm-9">
+					<div class="post-body">
+								<h6>December 27, 2014</h6>
+								<h2 >Modern hang-ups with wallcoverings</h2>
+								<p>
+									Paper makes rebirth in home decor BY REBECCA KEILLOR, POSTMEDIA NEWS Everything about wallpaper has changed, including its name. Now referred to as wallcoverings by designers – as it no longer...								</p>
+							    <div class="post-sign">By
+								<span class="author"><a href="#">Exclusive Wallcovering</a></span>in
+								<span class="category"><a href="#" rel="category tag">Blog</a>, <a href="#" rel="category tag">Featured</a></span>
+								</div>
+								</div>
+							</div>
+					
+					</div>
+					<br /> 
+					<br /> 
+					<br /> 
+					
+					<div class="row"> 
+					<div class="col-sm-3"> 
+						<img src="img/92.jpg" class="img-fluid" alt="Responsive image">
+					</div>
+					<div class="col-sm-9">
+					<div class="post-body">
+								<h6>November 22, 2014</h6>
+								<h2 >Adding wallcoverings can spruce up your home</h2>
+								<p>
+									By CHRISTINE BRUN CREATORS.COM Wallpaper can be an efficient and effective way to spruce up a house without taking up any room. Wall coverings have been around for hundreds of years, but admiration...							</p>
+							    <div class="post-sign">By
+								<span class="author"><a href="#">Exclusive Wallcovering</a></span>in
+								<span class="category"><a href="#" rel="category tag">Blog</a>, <a href="#" rel="category tag">Featured</a></span>
+								</div>
+								</div>
+							</div>
+					
+					</div>
+				</div>
+				<hr />
+		
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+		<script src="https://use.fontawesome.com/97a933fe64.js"></script>
+		</body>
+</html>
